6784db4b079463b33afb1278e6bbc229aa62aed9
Emmanuel Kirui <emmanuelkirui822@gmail.com>
2024-02-23 16:53:55 +0300

    first commit

---
+++ averag/src/Average.java
@@ -0,0 +1,38 @@
+import java.util.Scanner;
+public class Average {
+    public static void main(String[] args) {
+       try (Scanner scanner= new Scanner(System.in)){
+        System.out.println("Enter the size ");
+        int size=scanner.nextInt();
+        int[] array=new int[size];
+        System.out.println(" Enter the values");
+        for(int i= 0;i<size;i++){
+
+            int value=scanner.nextInt();
+            array[i]=value;
+
+
+        }
+        double length=array.length;
+        double sum=0;
+        for(int i=0;i<array.length;i++){
+            sum= array[i]+sum;
+
+
+        }
+        double average =sum/length;
+        System.out.println("The average is "+average);
+
+
+
+
+
+
+
+
+
+
+       }
+        
+    }
+}
